8355ea425d9260368ac67b6323f13dcd22d08d1d
Oluniyisrael <israeloluniyi2004@gmail.com>
2023-06-15 17:25:07 +0100

    Its comiing together small small

---
+++ index/index.js
@@ -0,0 +1,33 @@
+document.addEventListener("DOMContentLoaded", function(event) {
+    let slides = document.querySelectorAll(".slide");
+    let currentSlide = 0;
+  
+    function showSlide(n) {
+      slides[currentSlide].style.display = "none";
+      currentSlide = (n + slides.length) % slides.length;
+      slides[currentSlide].style.display = "block";
+    }
+  
+    function nextSlide() {
+      showSlide(currentSlide + 1);
+    }
+  
+    function previousSlide() {
+      showSlide(currentSlide - 1);
+    }
+  
+    document.querySelector(".prev").addEventListener("click", previousSlide);
+    document.querySelector(".next").addEventListener("click", nextSlide);
+  
+    document.addEventListener("keydown", function(e) {
+      if (e.key === "ArrowRight") {
+        nextSlide();
+      } else if (e.key === "ArrowLeft") {
+        previousSlide();
+      }
+    });
+  
+    setInterval(nextSlide, 5000); // Change slide every 5 seconds
+    showSlide(currentSlide);
+  });
+  
